4fc9d40bfd0a72151f8fd7b17ce0e93170811fbe
zxysilent <zxysilent@outlook.com>
2019-04-04 09:49:51 +0800

    fix

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 const resolve = dir => {
 	return path.join(__dirname, dir);
