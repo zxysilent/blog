9cd5f4f87f63e8dfdd8371415da1534ae1f87227
zxysilent <zxysilent@outlook.com>
2019-08-19 10:50:34 +0800

    更新登陆页面

--- vue/src/app.vue
+++ vue/src/app.vue
@@ -13,6 +13,7 @@ body {
 	height: 100%;
 	background: #f0f0f0;
 	overflow: auto;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
 #app {
 	width: 100%;
--- vue/src/views/login.vue
+++ vue/src/views/login.vue
@@ -1,34 +1,54 @@
 <style lang="less" scoped>
 @import "./login.less";
 </style>
-
 <template>
-	<div class="login" @keydown.enter="handleSubmit">
-		<div class="login-ctn">
-			<Card shadow>
-				<p slot="title">
-					<Icon type="md-log-in" />
-					欢迎登录
-				</p>
-				<Form ref="loginForm" :model="user" :rules="rules">
-					<FormItem prop="num" label="用户名">
-						<Input prefix="ios-person-outline" type="text" v-model="user.num" placeholder="请输入用户名">
-						</Input>
-					</FormItem>
-					<FormItem prop="pass" label="密码">
-						<Input prefix="ios-lock-outline" type="password" v-model="user.pass" placeholder="请输入密码">
-						</Input>
-					</FormItem>
-					<FormItem>
-						<Button @click="handleSubmit" type="primary" long>登 录</Button>
-					</FormItem>
-				</Form>
-				<p class="login-ctn-tip">首页
-					<a href="/" title="首页">
-						<Icon type="ios-send-outline" size="20" />
-					</a>
-				</p>
-			</Card>
+	<div class="login" @keydown.enter="submit">
+		<div class="top">
+			<div class="header">
+				<a>
+					<img src="../assets/logo.png" class="logo" alt="logo">
+					<span class="title">欢迎登陆</span>
+				</a>
+			</div>
+			<div class="desc">
+				github.com/zxysilent/blog
+			</div>
+		</div>
+		<div class="main">
+			<Form ref="loginForm" :model="user" :rules="rules">
+				<FormItem prop="num" label="用户名">
+					<Input size="large" prefix="ios-person-outline" type="text" v-model="user.num" placeholder="请输入用户名">
+					</Input>
+				</FormItem>
+				<FormItem prop="pass" label="密码">
+					<Input size="large" prefix="ios-lock-outline" type="password" v-model="user.pass" placeholder="请输入密码">
+					</Input>
+				</FormItem>
+				<FormItem>
+					<Button size="large" @click="submit" type="primary" long>登 录</Button>
+				</FormItem>
+			</Form>
+			<p class="login-tip">首页
+				<a href="/" title="首页">
+					<Icon type="ios-send-outline" size="20" />
+				</a>
+			</p>
+			<div class="other-login">
+				<span>其他登录方式</span>&nbsp;
+				<a>
+					<Icon type="logo-github" />
+				</a>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="links">
+				<a>帮助</a>
+				<a>隐私</a>
+				<a>条款</a>
+			</div>
+			<div class="copyright">
+				Copyright &copy; {{new Date().getFullYear()}}&nbsp;<a target="_blank" href="https://github.com/zxysilent">github.com/zxysilent</a>&nbsp;&nbsp;<a target="_blank" href="https://blog.zxysilent.com">blog.zxysilent.com</a>
+			</div>
 		</div>
 	</div>
 </template>
@@ -47,7 +67,7 @@ export default {
 		};
 	},
 	methods: {
-		handleSubmit() {
+		submit() {
 			let that = this;
 			that.$refs.loginForm.validate(valid => {
 				if (valid) {
