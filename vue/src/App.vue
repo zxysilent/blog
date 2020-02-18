c5b5b29a1265124203581c91d6c33fe8d8412c1b
zxysilent <zxysilent@outlook.com>
2020-02-18 19:29:54 +0800

    [F] 修改大小写end

---
+++ vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+html,
+body {
+	width: 100%;
+	height: 100%;
+	background: #f0f0f0;
+	overflow: auto;
+	font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial,sans-serif;
+}
+#app {
+	width: 100%;
+	height: 100%;
+}
+</style>
