d90e50ca0b655b3a1f72f9ad3ac4590564989059
zxysilent <zxysilent@outlook.com>
2020-03-22 16:29:27 +0800

    [U] 升级xorm

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module blog
 go 1.13
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
@@ -10,5 +11,5 @@ require (
 	github.com/swaggo/echo-swagger v0.0.0-20191205130555-62f81ea88919
 	github.com/swaggo/swag v1.6.5
 	github.com/zxysilent/utils v0.0.0-20191216052648-b4e160be1894
-	xorm.io/xorm v0.8.1
+	xorm.io/xorm v1.0.0
 )
--- model/model.go
+++ model/model.go
@@ -7,6 +7,7 @@ import (
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"xorm.io/xorm"
+	"xorm.io/xorm/caches"
 
 	// 数据库驱动
 	_ "github.com/go-sql-driver/mysql"
@@ -17,24 +18,23 @@ var Db *xorm.Engine
 
 func Init() {
 	// 初始化数据库操作的 Xorm
-	db, err := xorm.NewEngine("mysql", conf.Dsn.Dsn())
+	db, err := xorm.NewEngine("mysql", conf.App.Dsn())
 	if err != nil {
 		log.Fatalln("数据库 dsn:", err.Error())
 	}
 	if err = db.Ping(); err != nil {
 		log.Fatalln("数据库 ping:", err.Error())
 	}
-	db.SetMaxIdleConns(conf.Xorm.Idle)
-	db.SetMaxOpenConns(conf.Xorm.Open)
+	db.SetMaxIdleConns(conf.App.Xorm.Idle)
+	db.SetMaxOpenConns(conf.App.Xorm.Open)
 	// 是否显示sql执行的语句
-	db.ShowSQL(conf.Xorm.Show)
-	db.ShowExecTime(conf.Xorm.Show)
-	if conf.Xorm.Cache.Enable {
+	db.ShowSQL(conf.App.Xorm.Show)
+	if conf.App.Xorm.CacheEnable {
 		// 设置xorm缓存
-		cacher := xorm.NewLRUCacher(xorm.NewMemoryStore(), conf.Xorm.Cache.Count)
+		cacher := caches.NewLRUCacher(caches.NewMemoryStore(), conf.App.Xorm.CacheCount)
 		db.SetDefaultCacher(cacher)
 	}
-	if conf.Xorm.Sync {
+	if conf.App.Xorm.Sync {
 		err := db.Sync2(new(User), new(Cate), new(Tag), new(Post), new(PostTag), new(Opts))
 		if err != nil {
 			log.Fatalln("数据库 sync:", err.Error())
