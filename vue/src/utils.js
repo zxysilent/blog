346de431a0c08af2b30b1e92e9fdefc7127dc3d8
zxysilent <zxysilent@outlook.com>
2021-04-15 12:48:27 +0800

    [D] 清理代码

--- vue/src/utils.js
+++ vue/src/utils.js
@@ -15,9 +15,7 @@ utils.setItem = (k, v) => {
 utils.getItem = k => {
 	return storage.getItem(k);
 };
-utils.clearItem = k => {
-	storage.removeItem(k);
-};
+
 utils.clearData = () => {
 	storage.clear();
 };
@@ -27,19 +25,5 @@ utils.setToken = token => {
 utils.getToken = () => {
 	return utils.getItem("bearer");
 };
-// 获取保存的用户信息
-utils.getAuth = () => {
-	try {
-		let token = Base64.decode(utils.getItem("bearer").split(".")[1]);
-		let auth = JSON.parse(token);
-		if (!auth.hasOwnProperty("id")) {
-			utils.removeItem("bearer");
-			location.href = "/login";
-		}
-		return auth;
-	} catch (e) {
-		utils.removeItem("bearer");
-		location.href = "/login";
-	}
-};
+
 export default utils;
