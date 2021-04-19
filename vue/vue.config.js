67d1bbb9408ea5a12761fdec73e8b048c352b54a
zxysilent <zxysilent@outlook.com>
2021-04-19 13:43:33 +0800

    [D] 无用

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
 			entry: "src/main.js", // page 的入口,每个"page"应该有一个对应的 JavaScript 入口文件
 			template: "public/index.html", // 模板来源
 			filename: "index.html", // 在 dist/index.html 的输出
-			title: "blog-管理", // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
+			title: "blog", // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
 			chunks: ["chunk-vendors", "chunk-common", "index"] // 在这个页面中包含的块，默认情况下会包含,提取出来的通用 chunk 和 vendor chunk
 		}
 	},
