1849849637fbc9e2bc878a968fd042e24755796f
zxysilent <zxysilent@outlook.com>
2021-04-21 08:43:23 +0800

    [U] hwt_test

--- internal/hwt/hwt_test.go
+++ internal/hwt/hwt_test.go
@@ -10,12 +10,12 @@ func TestEncode(t *testing.T) {
 		Id:     1,
 		RoleId: 1,
 		Num:    "账号",
-		ExpAt:  time.Now().Add(time.Hour * 2).Unix(),
+		ExpAt:  time.Now().Add(time.Hour * 24).Unix(),
 	}
 	t.Log(auth.Encode("key"))
 }
 func TestVerify(t *testing.T) {
-	raw := "eyJpIjoxLCJuIjoi6LSm5Y-3IiwiciI6MTAwMCwicmkiOjEsImUiOjE2MDk0MDMzOTV9.T1qLUcNedJYVHdxuLV_JtQ"
+	raw := "eyJpIjoxLCJuIjoi6LSm5Y-3IiwicmkiOjEsImUiOjE2MTkwNTIxNDF9.BgqifIFamVp_A_IEQ9lSIw"
 	auth := Auth{}
 	err := auth.Decode(raw, "key")
 	t.Log(auth, err)
@@ -32,7 +32,7 @@ func BenchmarkEncode(b *testing.B) {
 	}
 }
 func BenchmarkVerify(b *testing.B) {
-	raw := "eyJpIjoxLCJuIjoi6LSm5Y-3IiwiciI6MTAwMCwicmkiOjEsImUiOjE2MDk0MDMzOTV9.T1qLUcNedJYVHdxuLV_JtQ"
+	raw := "eyJpIjoxLCJuIjoi6LSm5Y-3IiwicmkiOjEsImUiOjE2MTkwNTIxNDF9.BgqifIFamVp_A_IEQ9lSIw"
 	auth := Auth{}
 	for i := 0; i < b.N; i++ {
 		auth.Decode(raw, "key")
