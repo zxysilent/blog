bd9b7268222c642a86bca3e15d0f93759e1de6aa
zxysilent <zxysilent@outlook.com>
2021-04-26 08:38:29 +0800

    [U] 请求方式

--- vue/src/api/tag.js
+++ vue/src/api/tag.js
@@ -1,17 +1,31 @@
 import fetch from "./fetch";
 // 标签信息
 export const apiTagAll = () => {
-	return fetch.get("/api/tag/all");
+	return fetch.request({
+		url: "/api/tag/all",
+		method: "get"
+	});
 };
 // 添加标签
 export const admTagAdd = data => {
-	return fetch.post(`/adm/tag/add`, data);
+	return fetch.request({
+		url: "/adm/tag/add",
+		method: "post",
+		data: data
+	});
 };
 // 修改标签
 export const admTagEdit = data => {
-	return fetch.post(`/adm/tag/edit`, data);
+	return fetch.request({
+		url: "/adm/menu/tree",
+		method: "post",
+		data: data
+	});
 };
 // 删除标签
 export const admTagDrop = id => {
-	return fetch.get(`/adm/tag/drop/${id}`);
+	return fetch.request({
+		url: `/adm/tag/drop/${id}`,
+		method: "get"
+	});
 };
