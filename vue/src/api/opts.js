e94846f2fa6dfcf8833f7d5d647881a279b2ff6a
zxysilent <zxysilent@outlook.com>
2021-05-01 20:27:45 +0800

    [U] 统一请求

--- vue/src/api/opts.js
+++ vue/src/api/opts.js
@@ -1,13 +1,23 @@
 import fetch from "./fetch";
 // 获取基本配置项
 export const apiOptsBase = () => {
-	return fetch.get(`/api/opts/base`);
+	return fetch.request({
+		url: "/api/opts/base",
+		method: "get"
+	});
 };
 // 获取某个配置项
 export const apiOptsGet = key => {
-	return fetch.get(`/api/opts/${key}`);
+	return fetch.request({
+		url: `/api/opts/${key}`,
+		method: "get"
+	});
 };
 // 编辑某个配置项
 export const admOptsEdit = data => {
-	return fetch.post("/adm/opts/edit", data);
+	return fetch.request({
+		url: "/adm/opts/edit",
+		method: "post",
+		data: data
+	});
 };
