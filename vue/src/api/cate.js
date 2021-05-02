fda15cd64277aff7ded4c9a9305716b724b88afe
zxysilent <zxysilent@outlook.com>
2021-05-02 10:39:22 +0800

    [U] 统一请求

--- vue/src/api/cate.js
+++ vue/src/api/cate.js
@@ -1,17 +1,31 @@
 import fetch from "./fetch";
 // 分类信息
 export const apiCateAll = () => {
-	return fetch.get("/api/cate/all");
+    return fetch.request({
+		url: "/api/cate/all",
+		method: "get"
+	});
 };
 // 添加分类
 export const admCateAdd = data => {
-	return fetch.post(`/adm/cate/add`, data);
+    return fetch.request({
+		url: "/adm/cate/add",
+		method: "post",
+        data:data
+	});
 };
 // 修改分类
 export const admCateEdit = data => {
-	return fetch.post(`/adm/cate/edit`, data);
+    return fetch.request({
+		url: "/adm/cate/edit",
+		method: "post",
+        data:data
+	});
 };
 // 删除分类
 export const admCateDrop = id => {
-	return fetch.get(`/adm/cate/drop/${id}`);
+    return fetch.request({
+		url: `/adm/cate/drop/${id}`,
+		method: "get"
+	});
 };
