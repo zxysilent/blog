c3376a150f8d5745a83afb9a1733bf872e70645e
zxysilent <zxysilent@outlook.com>
2021-07-18 23:29:07 +0800

    [U] 注释

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
 // @Title Blog’s 接口文档
 // @Version 1.0
 // @Description token传递方式包括[get/post]token、[header]Authorization
-// @Description 数据传递方式包括json、formData推荐使用json
-// @Description 使用json时token请使用[header]
 // @Description /api/* 公共访问
 // @Description /adm/* 必须传入 token
 // @Host 127.0.0.1:8085
