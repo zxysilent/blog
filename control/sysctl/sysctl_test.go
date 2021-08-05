c3f3cc1dff5957684c3508962a1afb81cb2b0f68
zxysilent <zxysilent@outlook.com>
2021-08-05 14:37:26 +0800

    [U] url编码

--- control/sysctl/sysctl_test.go
+++ control/sysctl/sysctl_test.go
@@ -10,6 +10,6 @@ import (
 // TestURLEncode url编码
 func TestURLEncode(t *testing.T) {
 	defer os.RemoveAll("logs")
-	u := "https://blog.zxysilent.com/auth/login/qq.html"
+	u := "https://blog.zxysilent.com/auth/qq.html"
 	log.Println(url.QueryEscape(u))
 }
