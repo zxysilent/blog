bc3c18007a5a9d7424f1cbfdea88981be744d8cd
zxysilent <zxysilent@outlook.com>
2021-08-05 10:57:46 +0800

    [A] 小工具

---
+++ control/sysctl/sysctl_test.go
@@ -0,0 +1,15 @@
+package sysctl
+
+import (
+	"log"
+	"net/url"
+	"os"
+	"testing"
+)
+
+// TestURLEncode url编码
+func TestURLEncode(t *testing.T) {
+	defer os.RemoveAll("logs")
+	u := "https://blog.zxysilent.com/auth/login/qq.html"
+	log.Println(url.QueryEscape(u))
+}
